c2e9e1e816d067c33ddf96aeb3fa9ecf26e94b16
an1skat <156462349+an1skat@users.noreply.github.com>
2024-12-24 19:59:16 +0200

    add js logic for header dropdown

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,51 +1,77 @@
 export const initModals = () => {
-  const welcomeModal = document.querySelector(".welcome-modal");
-//   const thanksModal = null;
-  const welcomeText = document.querySelector(".welcome-text");
-  const overlay = document.querySelector("[data-modal-overlay]");
-  const modalForm = document.querySelector(".modal-form");
+  const initWelcomeModal = () => {
+    const welcomeModal = document.querySelector(".welcome-modal");
+    //   const thanksModal = null;
+    const welcomeText = document.querySelector(".welcome-text");
+    const overlay = document.querySelector("[data-modal-overlay]");
+    const modalForm = document.querySelector(".modal-form");
 
-  const savedName = localStorage.getItem("userName");
+    const savedName = localStorage.getItem("userName");
 
-  savedName ? (welcomeText.textContent = `Вітаємо,  ${savedName}!`) : showModal(welcomeModal, overlay);
+    savedName
+      ? (welcomeText.textContent = `Вітаємо,  ${savedName}!`)
+      : showModal(welcomeModal, overlay);
 
-  document.addEventListener("keydown", e => {
-    if (e.key === "Escape") {
-      closeModal(welcomeModal, overlay);
-    //   closeModal(thanksModal);
-    }
-  });
-  document.querySelectorAll("[data-modal-close]").forEach(btn => {
-    btn.addEventListener("click", () => {
-      const modal = btn.closest(".modal");
-      closeModal(modal, overlay);
+    document.addEventListener("keydown", e => {
+      if (e.key === "Escape") {
+        closeModal(welcomeModal, overlay);
+        //   closeModal(thanksModal);
+      }
     });
-  });
-  overlay.addEventListener("click", () => {
-    closeModal(welcomeModal, overlay);
-    // closeModal(thanksModal);
-  });
-  welcomeText.addEventListener("click", () => {
-    showModal(welcomeModal, overlay);
-  });
+    document.querySelectorAll("[data-modal-close]").forEach(btn => {
+      btn.addEventListener("click", () => {
+        const modal = btn.closest(".modal");
+        closeModal(modal, overlay);
+      });
+    });
+    overlay.addEventListener("click", () => {
+      closeModal(welcomeModal, overlay);
+      // closeModal(thanksModal);
+    });
+    welcomeText.addEventListener("click", () => {
+      showModal(welcomeModal, overlay);
+    });
 
-  modalForm.addEventListener("submit", e => {
-    e.preventDefault();
-    const name = modalForm.querySelector(".modal-input").value.trim();
-    const nameRegex = /^[a-zA-Zа-яА-ЯёЁ]{4,20}$/;
+    modalForm.addEventListener("submit", e => {
+      e.preventDefault();
+      const name = modalForm.querySelector(".modal-input").value.trim();
+      const nameRegex = /^[a-zA-Zа-яА-ЯёЁ]{4,20}$/;
 
-    if (!nameRegex.test(name)) {
-      alert(
-        "Введіть нікнейм від 4 до 20 символів без викростання спеціальних символів"
-      );
-      return;
-    }
+      if (!nameRegex.test(name)) {
+        alert(
+          "Введіть нікнейм від 4 до 20 символів без викростання спеціальних символів"
+        );
+        return;
+      }
 
-    localStorage.setItem("userName", name);
-    welcomeText.textContent = `Вітаємо, ${name}!`;
-    closeModal(welcomeModal, overlay);
-    // showModal(thanksModal);
-  });
+      localStorage.setItem("userName", name);
+      welcomeText.textContent = `Вітаємо, ${name}!`;
+      closeModal(welcomeModal, overlay);
+      // showModal(thanksModal);
+    });
+  };
+
+  initWelcomeModal();
+
+  const initDropdown = () => {
+    document.querySelectorAll(".nav-item").forEach(item => {
+      const link = item.querySelector(".nav-link");
+      const dropdown = item.querySelector(".dropdown");
+
+      link.addEventListener("click", e => {
+        e.preventDefault();
+
+        const isActive = dropdown.classList.contains("active");
+        document
+          .querySelectorAll(".dropdown")
+          .forEach(dd => dd.classList.remove("active"));
+
+        if (!isActive) dropdown.classList.add("active");
+      });
+    });
+  };
+
+  initDropdown()
 };
 const showModal = (modal, overlay) => {
   modal.classList.remove("is-hidden");
