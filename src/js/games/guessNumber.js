0a9f5c035b56a7278b7674cc8ba1d8f5d8c27a38
egouhj5 <kn09032010@gmail.com>
2025-01-23 22:20:57 +0200

    guess the number js

--- src/js/games/guessNumber.js
+++ src/js/games/guessNumber.js
@@ -1,1 +1,27 @@
-export default class GuessNumber {}
+export default class guessNumber {
+  constructor() {
+    this.activateButton = document.querySelector("[data-guess-btn]")
+
+    this.init();
+  }
+
+  init() {
+    this.activateButton.addEventListener("click", this.guessNumber.bind(this));
+  }
+
+  guessNumber() {
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+    const paragraph = document.querySelector('[data-guess-result]')
+    const numberInput = document.querySelector('[data-guess-input]');
+    if (numberInput.value == randomNumber) {
+      paragraph.style.color = "green";
+      paragraph.textContent = "Ви вгадали число!";
+      console.log("right");
+    } else {
+      paragraph.style.color = "red";
+      paragraph.textContent = "Ви не вгадали число!";
+      console.log("wrong");
+    }
+    console.log(randomNumber);
+    }
+  }
